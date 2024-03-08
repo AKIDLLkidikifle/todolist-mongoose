eb7bee7b8831518e51d18e4d45a24e9bc703f1f0
kidist kifle <104790637+AKIDLLkidikifle@users.noreply.github.com>
2024-03-08 11:24:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 const date = require(__dirname+"/date.js")
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://USER_NAME:PASSWORD@cluster0.hbkdpsr.mongodb.net/todolistDB");
+mongoose.connect("mongodb+srv://admin_kidist:test_123@cluster0.hbkdpsr.mongodb.net/todolistDB");
 
 const app = express();
 
